ab3a9b811528457a3e7e9080e42ae85098752156
Jas Chen <jas228@gmail.com>
2016-11-05 10:35:29 +0800

    Move logMsg() into index.js

--- src/index.js
+++ src/index.js
@@ -96,3 +96,26 @@ export function caseOf(msg, ...params) {
     }
   }
 }
+
+// https://davidwalsh.name/javascript-arguments
+function getArgsNames(func) {
+  // First match everything inside the function argument parens.
+  var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+ 
+  // Split the arguments string into an array comma delimited.
+  return args.split(',').map(function(arg) {
+    // Ensure no inline comments are parsed and trim the whitespace.
+    return arg.replace(/\/\*.*\*\//, '').trim();
+  }).filter(function(arg) {
+    // Ensure no undefined values are added.
+    return arg;
+  });
+}
+
+export function logMsg(msg, log) {
+  const payload = {};
+  getArgsNames(msg.constructor).forEach((n, i) => payload[n] = msg.args[i]);
+  if (!log) console.log(msg.constructor.name, payload);
+  else if (typeof log === 'function') log(msg.constructor.name, payload);
+  else if (typeof log === 'string') console[log](msg.constructor.name, payload);
+}
